a3b22f1f0aaf095db90c372fec7878a4cb89f581
Valentijn van Zwieten <V.L.vanzwieten@students.uu.nl>
2018-12-09 18:05:39 +0100

    wat is deze import

--- lab2/main.cs
+++ lab2/main.cs
@@ -5,38 +5,44 @@ using System.Collections.Generic;
 namespace lab2 {
     public class Program {
         static void Main(string[] args) {
+            // create a new instance with given args
             new Instance(args);
         }
     }
 
     public class Instance {
-        private int myPort;
+        public int port;
         private int[] neighbourPorts;
+        private RoutingTable routing;
 
         public Instance(string[] ports) {
-            // get own port
-            this.myPort = Int32.Parse(ports[0]);
+            // get own port and start listening
+            Console.Title = "NetChange " + ports[0];
+            this.port = Int32.Parse(ports[0]);
+            new Listener(this.port);
 
             // list all neighbour ports
             this.neighbourPorts = new int[ports.Length - 1];
-            for (int i = 1; i < ports.Length; i++)
-                this.neighbourPorts[i - 1] = Int32.Parse(ports[i]);
+            for (int i = 1; i < ports.Length; i++) this.neighbourPorts[i - 1] = Int32.Parse(ports[i]);
+
+            // instanciate routing table
+            this.routing = new RoutingTable(port, neighbourPorts);
         }
 
         private void ShowTable() {
-
+            // todo
         }
 
         private void Send() {
-
+            // todo
         }
 
         private void CreateConnection() {
-
+            // todo
         }
 
         private void DestroyConnection() {
-
+            // todo
         }
     }
 }
---
+++ lab2/network.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Threading;
+using System.Net.Sockets;
+
+namespace lab2 {
+    class Listener {
+        public Listener(int port) {
+            // listen on given port
+            TcpListener server = new TcpListener(IPAddress.Any, port);
+            server.Start();
+
+            // start a thread for accepting connections
+            new Thread(() => CreateConnection(server)).Start();
+        }
+
+        private void CreateConnection(TcpListener handle) {
+            loop:
+
+            // create readers for new connection
+            TcpClient newNeighbour = handle.AcceptTcpClient();
+            StreamReader neighbourIn = new StreamReader(newNeighbour.GetStream());
+            StreamWriter neighbourOut = new StreamWriter(newNeighbour.GetStream());
+            neighbourOut.AutoFlush = true;
+
+            // read connecting port
+            int neighbourPort = int.Parse(neighbourIn.ReadLine());
+
+            // add connection to routing table
+            // todo
+
+            Console.WriteLine("Verbonden: " + neighbourPort);
+
+            goto loop;
+        }
+    }
+
+    class Connection {
+        public StreamReader Read;
+        public StreamWriter Write;
+
+        // constructor for client
+        public Connection(int port, int neightbourPort) {
+            // initiate reader and writer
+            TcpClient client = new TcpClient("localhost", neightbourPort);
+            this.Read = new StreamReader(client.GetStream());
+            this.Write = new StreamWriter(client.GetStream()) { Write.AutoFlush = true };
+
+            // tell neighbour our port
+            Write.WriteLine(port);
+
+            new Thread(Reader).Start();
+        }
+
+        // constructor for server
+        public Connection(StreamReader read, StreamWriter write) {
+            this.Read = read;
+            this.Write = write;
+
+            new Thread(Reader).Start();
+        }
+
+        // print whatever is read
+        public void Reader() {
+            try { while (true) Console.WriteLine(Read.ReadLine()); }
+            catch { } // todo print verbroken
+        }
+    }
+}
