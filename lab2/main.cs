28c95c2f0d41cff5a089e34b890df15946637971
jackckck <33979437+jackckck@users.noreply.github.com>
2018-12-09 12:02:04 +0100

    initial

---
+++ lab2/main.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Text;
+using System.Threading;
+using System.Collections.Generic;
+
+namespace lab2 {
+    public class Program {
+        static void Main(string[] args) {
+
+        }
+    }
+
+    public class NetChange {
+        private int myPort;
+        private int[] neighbourPorts;
+
+        public NetChange(string[] ports) {
+            this.myPort = Int32.Parse(ports[0]);
+
+            this.neighbourPorts = new int[ports.Length - 1];
+            for (int i = 1; i < ports.Length; i++)
+                this.neighbourPorts[i - 1] = Int32.Parse(ports[i]);
+
+        }
+        public NetChange (int[] ports) {
+            this.myPort = ports[0];
+
+            this.neighbourPorts = new int[ports.Length - 1];
+            for (int i = 1; i < ports.Length; i++)
+                this.neighbourPorts[i - 1] = ports[i];
+        }
+    }
+}
