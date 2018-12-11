c11949f9d2db029e48694a1496ceee37aab26de1
jackckck <33979437+jackckck@users.noreply.github.com>
2018-12-11 15:14:38 +0100

    iets meer aan routing toegevoegd

--- lab2/routing.cs
+++ lab2/routing.cs
@@ -3,17 +3,26 @@ using System.Collections.Generic;
 
 namespace lab2 {
     public class RoutingTable {
-        private int originPort;
+        private int homePort;
+        private int nodeCount;
+
+        // key ^= neighbour's port int, value ^= Connection to neighbour
+        private Dictionary<int, Connection> neighbourConnections = new Dictionary<int, Connection>();
+        // key ^= neighbour's port int, value ^= dictionary for which: key ^= node, value ^= neighbour's distance to node
+        private Dictionary<int, Dictionary<int, int>> neighbourSteps = new Dictionary<int, Dictionary<int, int>>();
         // if route = routes[key], then route[0] is the amount of steps, and route[1] the preferred neighbour
         private Dictionary<int, int[]> routes = new Dictionary<int, int[]>();
-        // if route = foreignRoutes[key], then route is the amount of steps between ports key[0] and key[1]
-        private Dictionary<int[], int> steps = new Dictionary<int[], int>();
 
-        public RoutingTable(int origin, int[] neighbours) {
-            this.originPort = origin;
-            // routes to direct neighbours
-            for (int i = 0; i < neighbours.Length; i++)
-                this.routes.Add(neighbours[i], new int[2] { 1, neighbours[i] });
+        public RoutingTable(int home, int[] neighbours) {
+            this.homePort = home;
+            this.nodeCount = 1 + neighbours.Length;
+            // connections and routes to direct neighbours
+            for (int i = 0; i < neighbours.Length; i++) {
+                this.neighbourConnections.Add(neighbours[i], new Connection(home, neighbours[i]));
+                this.neighbourSteps.      Add(neighbours[i], new Dictionary<int, int>());
+                this.routes.              Add(home, new int[2] { 0, home });
+                this.routes.              Add(neighbours[i], new int[2] { 1, neighbours[i] });
+            }
         }
 
         public int[] TryGetRoute(int port) {
@@ -23,17 +32,37 @@ namespace lab2 {
             return res;
         }
 
-        public int[] TryGetSteps(int portA, int portB) {
-            int res = -1;
-            if (!this.steps.TryGetValue(new int[2] { portA, portB }, out res)) {
-                this.steps.TryGetValue(new int[2] { portB, portA }, out res);
+        public void UpdateNeighbourSteps(int neighbour, int port, int steps) {
+            if (!this.neighbourSteps.ContainsKey(neighbour)) {
+                Console.WriteLine("No neighbour found at " + neighbour);
+            } else {
+                if (this.neighbourSteps[neighbour].ContainsKey(port))
+                    this.neighbourSteps[neighbour][port] = steps;
+                else
+                    this.neighbourSteps[neighbour].Add(port, steps);
             }
-            return res;
+        }
+
+        public void Recompute(int port) {
+            int rSteps, rNb;
+            if (this.homePort == port) {
+                rSteps = 0; rNb = homePort;
+            } else {
+                // TODO opvragen
+            }
+            // this.routes[port] = new int[2] { rSteps, rNb };
         }
 
         public override string ToString() {
-            // todo implement
-            return "todo";
+            string res = "";
+            foreach (KeyValuePair<int, int[]> route in this.routes) {
+                res += route.Key + " " + route.Value[0] + " ";
+                // port == eigen port
+                if (route.Key == route.Value[1]) res += "local";
+                else res += route.Value[1];
+                res += "\n";
+            }
+            return res;
         }
     }
 }
