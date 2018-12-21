cb3ddf92ace006b673b1a443349fdd72de2dba83
Valentijn van Zwieten <V.L.vanzwieten@students.uu.nl>
2018-12-21 14:59:03 +0100

    a

--- lab2/main.cs
+++ lab2/main.cs
@@ -66,7 +66,7 @@ namespace lab2 {
             routing.GetConnection(farPort).Send(message);
         }
 
-        // process an incoming message
+        // todo process an incoming message
         private void ProcessMessage(string message) {
             switch (message[0]) {
                 // new connection from neighbour 
